91d08faca4b764ccd6387db6713ecbd2f0754db1
sbx_user1051 <sbx_user1051@169.254.89.29>
2022-10-24 13:27:23 +0000

    Update requested for Proton - service/sample-lambda-function-template/service-instance/sample-lambda-function-template

---
+++ sample-vpc-environment-template/sample-lambda-function-template-sample-lambda-function-template/outputs.tf
@@ -0,0 +1,11 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:990828982247:service/sample-lambda-function-template/service-instance/sample-lambda-function-template
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+output "lambda_arn" {
+  value = "whocares"
+}
