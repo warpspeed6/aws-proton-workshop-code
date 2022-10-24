bef79bd0467e0e5ce8fa0ed08bf119c80bc420cb
sbx_user1051 <sbx_user1051@169.254.137.225>
2022-10-24 13:59:27 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

--- sample-vpc-environment-template/outputs.tf
+++ sample-vpc-environment-template/outputs.tf
@@ -10,12 +10,8 @@ output "vpc_arn" {
   value = module.vpc.vpc_arn
 }
 
-output "private_subnet_id" {
-  value          = [for az, subnet in aws_subnet.private: subnet.id]
-}
-  
-output "public_subnet_id" {
-  value          = [for az, subnet in aws_subnet.public: subnet.id]
+output "subnet_id" {
+  value = one(module.vpc.private_subnets) # there is a known issue with terraform lists as outputs for proton
 }
 
 output "security_group_id" {
